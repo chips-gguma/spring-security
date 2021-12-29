09b60efd241ef4c580f23de78fb3b6b0cf92e875
chips-gguma <ljygguma@gmail.com>
2021-12-29 17:09:09 +0900

    user와 admin 추가, 패스워드 인코더 생성

--- server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/basic-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,10 +1,38 @@
 package com.sp.fc.web.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(prePostEnabled = true) // prePost로 권한 체크 수행 (admin 접근 불가)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // 사용자 추가
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser(User.builder()
+                .username("user2")
+                        .password(passwordEncoder().encode("2222"))
+                        .roles("USER")
+                ).withUser(User.builder()
+                        .username("admin")
+                        .password(passwordEncoder().encode("3333"))
+                        .roles("ADMIN"))
+                ;
+    }
+
+    // 패스워드 인코더로 인코딩되지 않아 발생하는 에러 방지
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
